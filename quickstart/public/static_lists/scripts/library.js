37f94d837146eaa9d7567349e7e0f754ba949383
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2024-12-14 18:28:17 +0100

    update

--- quickstart/public/static_lists/scripts/library.js
+++ quickstart/public/static_lists/scripts/library.js
@@ -11,7 +11,7 @@ let view_small_icons = false;
 let show_pure_links = true;
 let highlight_bookmarks = false;
 let object_list_data = null;
-let sort_function = null; // page_rating_votes, date_published
+let sort_function = "page_rating_votes"; // page_rating_votes, date_published
 let default_page_size = 200;
 
 
