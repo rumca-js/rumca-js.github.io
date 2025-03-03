da53d7a4f12055f37d8855b2fef7fb737a1fe4fb
X Y Z <rozbujnik@gmail.com>
2025-03-03 16:07:40 +0100

    Added script to filter sqlite db. Moved preview script to a file

--- utils/reflected.py
+++ utils/reflected.py
@@ -1,9 +1,20 @@
-from sqlalchemy import MetaData, Table, select
+from sqlalchemy import MetaData, Table, select, text, inspect
 
 
 class ReflectedEntryTable(object):
     def __init__(self, engine):
         self.engine = engine
+
+    def get_table(self, table_name):
+        destination_metadata = MetaData()
+        destination_table = Table(table_name, destination_metadata, autoload_with=self.engine)
+        return destination_table
+
+    def truncate_table(self, table_name):
+        with self.engine.connect() as connection:
+            sql_text = f"DELETE FROM {table_name};"
+            connection.execute(text(sql_text))
+            connection.commit()
 
     def get_entries(self):
         destination_metadata = MetaData()
@@ -17,6 +28,18 @@ class ReflectedEntryTable(object):
 
             for entry in entries:
                 yield entry
+
+    def get_source(self, source_id):
+        destination_metadata = MetaData()
+        destination_table = Table("sourcedatamodel", destination_metadata, autoload_with=self.engine)
+
+        stmt = select(destination_table).where(destination_table.c.id == source_id)
+
+        with self.engine.connect() as connection:
+            result = connection.execute(stmt)
+            rows = result.fetchall()
+            for row in rows:
+                return row
 
     def get_tags_string(self, entry_id):
         destination_metadata = MetaData()
@@ -33,7 +56,7 @@ class ReflectedEntryTable(object):
                 if tags:
                     tags += ", "
 
-                tags += row.tag
+                tags += "#"+row.tag
 
         return tags
 
@@ -52,3 +75,16 @@ class ReflectedEntryTable(object):
                 tags.append(row.tag)
 
         return tags
+
+    def close(self):
+        with self.engine.connect() as connection:
+            connection.execute(text("VACUUM"))
+
+    def print_summary(self):
+        inspector = inspect(self.engine)
+        tables = inspector.get_table_names()
+
+        with self.engine.connect() as connection:
+            for table in tables:
+                row_count = connection.execute(text(f"SELECT COUNT(*) FROM {table}")).scalar()
+                print(f"Table: {table}, Row count: {row_count}")
