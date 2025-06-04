7d5b44a8aba3af215c6bef85ad94190df9478155
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2025-06-04 09:31:35 +0200

    Update

--- data/filter_all.sh
+++ data/filter_all.sh
@@ -1,7 +1,6 @@
-cp ~/WorkDir/DjangoPage/data/backup_2025_05_05_sqlite/places/places.db .
-cp ~/WorkDir/DjangoPage/data/backup_2025_05_05_sqlite/catalog/catalog.db .
-cp ~/WorkDir/DjangoPage/data/backup_2025_05_05_sqlite/rsshistory/rsshistory.db .
-
+#cp ~/WorkDir/DjangoPage/data/backup_2025_05_05_sqlite/places/places.db .
+#cp ~/WorkDir/DjangoPage/data/backup_2025_05_05_sqlite/catalog/catalog.db .
+#cp ~/WorkDir/DjangoPage/data/backup_2025_05_05_sqlite/rsshistory/rsshistory.db .
 
 poetry run python filter.py --db places.db --votes -f places
 poetry run python filter.py --db catalog.db --bookmarked -f catalog
