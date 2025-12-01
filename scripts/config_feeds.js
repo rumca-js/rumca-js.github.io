4c1394c990b040968fe39610046c053b30bb69f3
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2025-12-01 16:56:53 +0100

    Added feeds

---
+++ scripts/config_feeds.js
@@ -0,0 +1,69 @@
+let worker = null;
+let db = null;
+let object_list_data = null;   // all objects lists
+let system_initialized = false;
+
+let view_display_type = "gallery";
+let view_display_style = "style-light";
+let view_show_icons = true;
+let view_small_icons = false;
+let user_age = 1;
+let debug_mode = false;
+
+let entries_direct_links = false;
+let highlight_bookmarks = false;
+let sort_function = "-page_rating_votes"; // page_rating_votes, date_published
+let default_page_size = 200;
+
+let entries_visit_alpha=1.0;
+let entries_dead_alpha=0.5;
+
+
+function getDefaultFileName() {
+    return "feeds.db.zip";
+}
+
+
+function getFileList() {
+    return ["top.zip",
+	    "bookmarks.zip",
+	    "music.zip",
+	    "internet.db.zip",
+    ];
+}
+
+
+function getDefaultFileLocation() {
+    return "/data/";
+}
+
+
+function getFileVersion() {
+    /* Forces refresh of the file */
+    return "71";
+}
+
+
+function getSystemVersion() {
+    return "1.0.3";
+}
+
+
+function getInitialSearchSuggestsions() {
+    return [
+        "tag = full album",
+        "tag = youtube mix",
+        "tag = hip-hop",
+        "tag = metal",
+        "tag = reggae",
+        "tag = big beat",
+        "tag = trance",
+        "tag = foreign",
+        "tag = chiptune",
+        "tag = keygen",
+        "language = fr",
+        "language = mn",
+        "language = pl",
+        "link = youtube.com",
+    ];
+}
