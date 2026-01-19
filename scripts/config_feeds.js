8c3d8ebe1db03fbb104ed9be9f06695db79b689b
X Y Z <rozbujnik@gmail.com>
2026-01-19 16:32:07 +0100

    Fixed script

--- scripts/config_feeds.js
+++ scripts/config_feeds.js
@@ -82,8 +82,36 @@ function getInitialSearchSuggestsions() {
     ];
 }
 
+
 function debug(text) {
     if (true) {
       console.log(text);
     }
+}
+
+
+function getViewStyles() {
+    return [
+        "standard",
+        "gallery",
+        "search-engine",
+        "content-centric",
+        "links-only",
+    ];
+}
+
+
+function getOrderPossibilities() {
+    return [
+        ['page_rating_votes', "Votes ASC"],
+        ['-page_rating_votes', "Votes DESC"],
+        ['page_rating_views', "Views ASC"],
+        ['-page_rating_views', "Views DESC"],
+        ['date_published', "Date published ASC"],
+        ['-date_published', "Date published DESC"],
+        ['followers_count', "Followers ASC"],
+        ['-followers_count', "Followers DESC"],
+        ['stars', "Stars ASC"],
+        ['-stars', "Stars DESC"],
+    ];
 }
